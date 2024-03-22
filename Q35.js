810a8afac6541f7d69217090dde0f2c7531fb2d3
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-22 14:22:37 -0700

    Add files via upload

---
+++ Q35.js
@@ -0,0 +1,11 @@
+"use strict";
+// 35. Animals: Think of at least three different animals that have a common characterstics. Store the names of these 
+// animals in a list, and then use a for loop to print out the name of each animal. Modify your prayer to print a 
+// statement about each animal, such as a dog make a great pet.
+// • Add a line at the end of your program stating what these animals have in comman. You could print a sentence such 
+// Any of these animals would make a great pet!
+let animals = ["dog", "cat", "tiger"];
+for (let animal of animals) {
+    console.log(`${animal} runs very fast`);
+}
+console.log("\n cat is a clean animal");
